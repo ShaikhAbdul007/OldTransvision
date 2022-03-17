8ee7cd0c02914bc8d29c2a5d515c9c4e6c539296
Abdul Rehman Shaikh <rehmanshaikh0796@gmail.com>
2022-03-17 17:45:57 +0530

    unuse import removes

--- lib/Screen/bookingscreen/bokigdtls.dart
+++ lib/Screen/bookingscreen/bokigdtls.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:transvision_app1/MyComponent/colors.dart';
 import 'package:transvision_app1/MyComponent/sizedbox.dart';
