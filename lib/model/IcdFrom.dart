e2b68a326ebcaa26aa45fc58038e5409dad66c18
Abdul Rehman Shaikh <rehmanshaikh0796@gmail.com>
2022-03-22 15:02:04 +0530

    icdform model done

---
+++ lib/model/IcdFrom.dart
@@ -0,0 +1,29 @@
+/// icd : "INAMG"
+/// icdname : "AMINGAON"
+
+class IcdFrom {
+  IcdFrom({
+      String? icd, 
+      String? icdname,}){
+    _icd = icd;
+    _icdname = icdname;
+}
+
+  IcdFrom.fromJson(dynamic json) {
+    _icd = json['icd'];
+    _icdname = json['icdname'];
+  }
+  String? _icd;
+  String? _icdname;
+
+  String? get icd => _icd;
+  String? get icdname => _icdname;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['icd'] = _icd;
+    map['icdname'] = _icdname;
+    return map;
+  }
+@override toString() => '$icdname';
+}
