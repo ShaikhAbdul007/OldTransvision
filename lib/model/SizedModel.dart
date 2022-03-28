0896124cdb3a3ee95e4c9f1c668058f110085513
Abdul Rehman Shaikh <rehmanshaikh0796@gmail.com>
2022-03-28 17:52:23 +0530

    Model Created

---
+++ lib/model/SizedModel.dart
@@ -0,0 +1,33 @@
+/// code : "DV"
+/// containerType : "DRY VAN"
+
+class SizedModel {
+  SizedModel({
+      String? code, 
+      String? containerType,}){
+    _code = code;
+    _containerType = containerType;
+}
+
+  SizedModel.fromJson(dynamic json) {
+    _code = json['code'];
+    _containerType = json['containerType'];
+  }
+  String? _code;
+  String? _containerType;
+SizedModel copyWith({  String? code,
+  String? containerType,
+}) => SizedModel(  code: code ?? _code,
+  containerType: containerType ?? _containerType,
+);
+  String? get code => _code;
+  String? get containerType => _containerType;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['code'] = _code;
+    map['containerType'] = _containerType;
+    return map;
+  }
+  @override toString()=> '$_containerType';
+}
