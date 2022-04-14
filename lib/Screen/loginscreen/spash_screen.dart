ca00d40a83ff5b906c5256c7a2a624cc5fca644b
Abdul Rehman Shaikh <rehmanshaikh0796@gmail.com>
2022-04-14 16:37:25 +0530

    Api Integate

---
+++ lib/Screen/loginscreen/spash_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:transvision_app1/MyComponent/text.dart';
+import 'login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.orange.shade100,
+        body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Lottie.network(
+                "https://assets7.lottiefiles.com/packages/lf20_zvqaeada.json",
+                animate: true,
+                addRepaintBoundary: true,
+                repeat: true,
+                alignment: Alignment.center),
+            Container(
+              padding: const EdgeInsets.only(left: 30, top: 5),
+              height: 200,
+              width: 300,
+              child: Stack(children: const [
+                Positioned(
+                    child: WeightText(
+                  text: "Trans Vision",
+                  size: 30.0,
+                  color: Colors.black,
+                )),
+                Positioned(
+                  top: 35.0,
+                  left: 105.0,
+                  child: NormalText(
+                      text: "Shipping PVT LTD",
+                      size: 18.0,
+                      color: Colors.black),
+                ),
+                SizedBox(height: 15.0),
+                Positioned(
+                  top: 70,
+                  left: 100,
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.orange,
+                    valueColor: AlwaysStoppedAnimation(Colors.orangeAccent),
+                  ),
+                )
+              ]),
+            )
+          ]),
+        ));
+  }
+}
---
+++ lib/model/billsummary.dart
@@ -0,0 +1,105 @@
+/// balamt : 0
+/// billdate : "24-08-2021 00:00:00"
+/// billno : 10287
+/// billtotals : 1585
+/// bldate : "15-08-2021 00:00:00"
+/// blno : 4257
+/// payamt : 1585
+/// tdsamt : 0
+/// vesselname : "PSL EAGLE"
+/// voyage : "PLE111"
+
+class Billsummary {
+  Billsummary({
+      int? balamt, 
+      String? billdate, 
+      int? billno, 
+      int? billtotals, 
+      String? bldate, 
+      int? blno, 
+      int? payamt, 
+      int? tdsamt, 
+      String? vesselname, 
+      String? voyage,}){
+    _balamt = balamt;
+    _billdate = billdate;
+    _billno = billno;
+    _billtotals = billtotals;
+    _bldate = bldate;
+    _blno = blno;
+    _payamt = payamt;
+    _tdsamt = tdsamt;
+    _vesselname = vesselname;
+    _voyage = voyage;
+}
+
+  Billsummary.fromJson(dynamic json) {
+    _balamt = json['balamt'];
+    _billdate = json['billdate'];
+    _billno = json['billno'];
+    _billtotals = json['billtotals'];
+    _bldate = json['bldate'];
+    _blno = json['blno'];
+    _payamt = json['payamt'];
+    _tdsamt = json['tdsamt'];
+    _vesselname = json['vesselname'];
+    _voyage = json['voyage'];
+  }
+  int? _balamt;
+  String? _billdate;
+  int? _billno;
+  int? _billtotals;
+  String? _bldate;
+  int? _blno;
+  int? _payamt;
+  int? _tdsamt;
+  String? _vesselname;
+  String? _voyage;
+Billsummary copyWith({  int? balamt,
+  String? billdate,
+  int? billno,
+  int? billtotals,
+  String? bldate,
+  int? blno,
+  int? payamt,
+  int? tdsamt,
+  String? vesselname,
+  String? voyage,
+}) => Billsummary(  balamt: balamt ?? _balamt,
+  billdate: billdate ?? _billdate,
+  billno: billno ?? _billno,
+  billtotals: billtotals ?? _billtotals,
+  bldate: bldate ?? _bldate,
+  blno: blno ?? _blno,
+  payamt: payamt ?? _payamt,
+  tdsamt: tdsamt ?? _tdsamt,
+  vesselname: vesselname ?? _vesselname,
+  voyage: voyage ?? _voyage,
+);
+  int? get balamt => _balamt;
+  String? get billdate => _billdate;
+  int? get billno => _billno;
+  int? get billtotals => _billtotals;
+  String? get bldate => _bldate;
+  int? get blno => _blno;
+  int? get payamt => _payamt;
+  int? get tdsamt => _tdsamt;
+  String? get vesselname => _vesselname;
+  String? get voyage => _voyage;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['balamt'] = _balamt;
+    map['billdate'] = _billdate;
+    map['billno'] = _billno;
+    map['billtotals'] = _billtotals;
+    map['bldate'] = _bldate;
+    map['blno'] = _blno;
+    map['payamt'] = _payamt;
+    map['tdsamt'] = _tdsamt;
+    map['vesselname'] = _vesselname;
+    map['voyage'] = _voyage;
+    return map;
+  }
+
+}
